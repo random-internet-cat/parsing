6c6ca5ad606505939e6693ac067472437c5f9867
Jason Cobb <jason.e.cobb@gmail.com>
2019-05-23 14:22:43 -0400

    Fixup defaults

--- TwurtleParser/include/randomcat/parser/detail/defaults.hpp
+++ TwurtleParser/include/randomcat/parser/detail/defaults.hpp
@@ -1,6 +1,6 @@
-`#pragma once
+#pragma once
 
-    namespace randomcat::parser {
+namespace randomcat::parser {
     using default_size_type = std::size_t;
     using default_priority_type = std::int32_t;
-}
+}    // namespace randomcat::parser
