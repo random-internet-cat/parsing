6eeccdf34bdb8dbc0af3c90d305ba2eff9039a46
Jason Cobb <jason.e.cobb@gmail.com>
2019-05-26 21:08:09 -0400

    Use custom tuple and variant types for grammars Provides similar interfaces to std::tuple and std::variant Allows access though the type of the grammar, not just index or result type

--- TwurtleParser/include/randomcat/parser/grammar/grammar_terms.hpp
+++ TwurtleParser/include/randomcat/parser/grammar/grammar_terms.hpp
@@ -4,6 +4,8 @@
 #include <tuple>
 #include <variant>
 
+#include <randomcat/type_container/type_list.hpp>
+
 #include "randomcat/parser/detail/util.hpp"
 #include "randomcat/parser/tokens/token_stream/token_stream.hpp"
 
@@ -52,13 +54,120 @@ namespace randomcat::parser {
         Matches m_matches;
     };
 
+    namespace grammar_variant_detail {
+        template<typename GrammarList, typename ValueList>
+        class grammar_variant;
+
+        template<typename... Grammars, typename... Values>
+        class grammar_variant<type_container::type_list<Grammars...>, type_container::type_list<Values...>> {
+        private:
+            using GrammarList = type_container::type_list<Grammars...>;
+            using ValueList = type_container::type_list<Values...>;
+
+            static_assert((util_detail::is_simple_type_v<Values> && ...));
+
+        public:
+            template<std::size_t I>
+            constexpr explicit grammar_variant(std::in_place_index_t<I>, type_container::type_list_at_t<ValueList, I> _val)
+            : m_value(std::in_place_index<I>, std::move(_val)) {}
+
+            template<std::size_t I>
+            constexpr auto get() const -> decltype(auto) {
+                return std::get<I>(this->m_value);
+            }
+
+            template<typename Grammar>
+            constexpr auto for_grammar() const -> decltype(get<type_container::type_list_first_index_of_v<GrammarList, Grammar>>()) {
+                static_assert(type_container::type_list_count_v<GrammarList, Grammar> == 1);
+                return get<type_container::type_list_first_index_of_v<GrammarList, Grammar>>();
+            }
+
+            constexpr auto const& underlying() const noexcept { return m_value; }
+
+            constexpr auto index() const noexcept { return m_value.index(); }
+
+        private:
+            std::variant<Values...> m_value;
+        };
+
+        template<std::size_t I, typename... VariantArgs>
+        inline constexpr auto get(grammar_variant<VariantArgs...> const& _value) -> decltype(_value.template get<I>()) {
+            return _value.template get<I>();
+        }
+
+        template<typename Grammar, typename... VariantArgs>
+        inline constexpr auto for_grammar(grammar_variant<VariantArgs...> const& _value) {
+            return _value.template for_grammar<Grammar>();
+        }
+
+        template<typename Visitor, typename... VariantArgs>
+        inline constexpr auto visit(Visitor&& _visitor, grammar_variant<VariantArgs...> const& _value)
+            -> decltype(std::visit(std::forward<Visitor>(_visitor), _value.underlying())) {
+            return std::visit(std::forward<Visitor>(_visitor), _value.underlying());
+        }
+    }    // namespace grammar_variant_detail
+
+    using grammar_variant_detail::for_grammar;
+    using grammar_variant_detail::get;
+    using grammar_variant_detail::visit;
+
+    namespace grammar_tuple_detail {
+        template<typename GrammarList, typename ValueList>
+        class grammar_tuple;
+
+        template<typename... Grammars, typename... Values>
+        class grammar_tuple<type_container::type_list<Grammars...>, type_container::type_list<Values...>> {
+        private:
+            static_assert((util_detail::is_simple_type_v<Values> && ...));
+
+            using GrammarList = type_container::type_list<Grammars...>;
+            using ValueList = type_container::type_list<Values...>;
+
+        public:
+            constexpr explicit grammar_tuple(Values... _values) noexcept : m_tuple(std::move(_values)...) {}
+
+            template<std::size_t I>
+            constexpr decltype(auto) get() const {
+                return std::get<I>(m_tuple);
+            }
+
+            template<typename Grammar>
+            constexpr decltype(auto) for_grammar() const {
+                static_assert(type_container::type_list_count_v<GrammarList, Grammar> == 1);
+                return get<type_container::type_list_first_index_of_v<GrammarList, Grammar>>();
+            }
+
+            constexpr auto const& underlying() const noexcept { return m_tuple; }
+
+        private:
+            std::tuple<Values...> m_tuple;
+        };
+
+        template<std::size_t I, typename... TupleArgs>
+        inline constexpr auto get(grammar_tuple<TupleArgs...> const& _tuple) -> decltype(_tuple.template get<I>()) {
+            return _tuple.template get<I>();
+        }
+
+        template<typename Grammar, typename... TupleArgs>
+        inline constexpr auto for_grammar(grammar_tuple<TupleArgs...> const& _tuple) -> decltype(_tuple.template for_grammar<Grammar>()) {
+            return _tuple.template for_grammar<Grammar>();
+        }
+    }    // namespace grammar_tuple_detail
+
+    using grammar_tuple_detail::for_grammar;
+    using grammar_tuple_detail::get;
+
     template<typename... SubGrammars>
     class sequence_grammar {
     public:
         template<typename TokenStream>
         struct traits_for {
-            using value_type = std::tuple<typename grammar_traits<SubGrammars, TokenStream>::value_type...>;
-            using error_type = std::variant<typename grammar_traits<SubGrammars, TokenStream>::error_type...>;
+            using value_type =
+                grammar_tuple_detail::grammar_tuple<type_container::type_list<SubGrammars...>,
+                                                    type_container::type_list<typename grammar_traits<SubGrammars, TokenStream>::value_type...>>;
+            using error_type =
+                grammar_variant_detail::grammar_variant<type_container::type_list<SubGrammars...>,
+                                                        type_container::type_list<typename grammar_traits<SubGrammars, TokenStream>::error_type...>>;
 
             using result_type = parse_result<value_type, error_type>;
         };
@@ -144,8 +253,12 @@ namespace randomcat::parser {
 
         template<typename TokenStream>
         struct traits_for {
-            using value_type = std::variant<typename grammar_traits<SubGrammars, TokenStream>::value_type...>;
-            using error_type = std::tuple<typename grammar_traits<SubGrammars, TokenStream>::error_type...>;
+            using value_type =
+                grammar_variant_detail::grammar_variant<type_container::type_list<SubGrammars...>,
+                                                        type_container::type_list<typename grammar_traits<SubGrammars, TokenStream>::value_type...>>;
+            using error_type =
+                grammar_tuple_detail::grammar_tuple<type_container::type_list<SubGrammars...>,
+                                                    type_container::type_list<typename grammar_traits<SubGrammars, TokenStream>::error_type...>>;
 
             using result_type = parse_result<value_type, error_type>;
         };
